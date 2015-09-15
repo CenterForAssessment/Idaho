0b40f0c211d5ba7500f5af8bbf3673c613292929
dbetebenner <dbetebenner@gmail.com>
2015-09-15 08:24:47 -0400

    Update of Idaho_SGP_2015 script

--- Idaho_SGP_2015.R
+++ Idaho_SGP_2015.R
@@ -23,7 +23,7 @@ load("Data/Idaho_SGP.Rdata")
 Idaho_2015.config <- list(
         ELA.2015 = list(
                 sgp.content.areas=c('READING', 'READING', 'READING', 'READING', 'ELA'),
-                sgp.panel.years=c('2010', '2011' '2012', '2013', '2015'),
+                sgp.panel.years=c('2010', '2011', '2012', '2013', '2015'),
                 sgp.grade.sequences=list(c('3', '5'), c('3', '4', '6'), c('3', '4', '5', '7'), c('3', '4', '5', '6', '8'), c('4', '5', '6', '7', '9'), c('5', '6', '7', '8', '10'))),
         MATHEMATICS.2015 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
@@ -36,6 +36,12 @@ Idaho_2015.config <- list(
 Idaho_SGP <- updateSGP(
 			Idaho_SGP,
 			Idaho_Data_LONG_2015,
+			sgp.percentiles=TRUE,
+			sgp.percentiles.baseline=FALSE,
+			sgp.projections=FALSE,
+			sgp.projections.baseline=FALSE,
+			sgp.projections.lagged=FALSE,
+			sgp.projections.lagged.baseline=FALSE,
 			sgp.config=Idaho_2015.config,
 			sgPlot.demo.report=TRUE)#,
 #			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SUMMARY=4, GA_PLOTS=4, SG_PLOTS=1)))
