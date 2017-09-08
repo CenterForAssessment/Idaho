5e2b6d98fba726b782d119c6f245e181f6f8b9be
dbetebenner <dbetebenner@gmail.com>
2017-09-08 05:20:17 -0400

    Update of SGP script

--- Idaho_SGP.R
+++ Idaho_SGP.R
@@ -18,12 +18,14 @@ load("Data/Idaho_Data_LONG.Rdata")
 
 Idaho_SGP <- abcSGP(
 		Idaho_Data_LONG,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "summarizeSGP", "visualizeSGP", "outputSGP"),
 		sgp.percentiles.baseline=FALSE,
 		sgp.projections.baseline=FALSE,
 		sgp.projections.lagged.baseline=FALSE,
 		save.intermediate.results=TRUE,
+		sgp.target.scale.scores=TRUE,
 		sgPlot.demo.report=TRUE,
-		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=2, PROJECTIONS=2, LAGGED_PROJECTIONS=2, SUMMARY=2, GA_PLOTS=2, SG_PLOTS=1)))
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=2, PROJECTIONS=2, LAGGED_PROJECTIONS=2, SGP_SCALE_SCORE_TARGETS=2, SUMMARY=2, GA_PLOTS=2, SG_PLOTS=1)))
 
 
 ### Save results
